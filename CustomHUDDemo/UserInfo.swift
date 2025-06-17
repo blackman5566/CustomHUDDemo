2f0c477d818d7ae517b7e7b7fd10745ff961505b
Allen Shiu <pt7922310@gmail.com>
2025-06-17 15:02:32 +0800

    Add UserInfo class

---
+++ CustomHUDDemo/UserInfo.swift
@@ -0,0 +1,20 @@
+//
+//  UserInfo.swift
+//  CustomHUDDemo
+//
+//  Created by Codex on 2025/5/5.
+//
+
+import Foundation
+
+class UserInfo {
+    var name: String
+    var userId: String
+    var userEmail: String
+
+    init(name: String, userId: String, userEmail: String) {
+        self.name = name
+        self.userId = userId
+        self.userEmail = userEmail
+    }
+}
